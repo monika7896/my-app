c4f43e729335a27240212d11f21e94c6b61e974b
monicas-kumari <monika.kumari@leptonmaps.com>
2024-01-26 22:49:37 +0530

    ok

--- projects/todo-app-project-one copy/src/component/TodoItems.jsx
+++ projects/todo-app-project-one copy/src/component/TodoItems.jsx
@@ -4,7 +4,7 @@ import TodoItem from './TodoItem'
 const TodoItems= () => {
   return (
     <>        
-    <TodoItem todoName='Buy Milk' todoDate='4/10/24'></TodoItem>
+    <TodoItem todoName='Buy k Milk' todoDate='4/10/24'></TodoItem>
     <TodoItem todoName='go to school' todoDate='4/11/24'></TodoItem>
       
     </>
